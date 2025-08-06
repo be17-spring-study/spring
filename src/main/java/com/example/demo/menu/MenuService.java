5e2e87a63e602e2677031d5ccecbb51337ac5528
young1042 <kyjfree00@gmail.com>
2025-08-06 17:39:53 +0900

    MenuService 11번 줄 오타수정

--- src/main/java/com/example/demo/menu/MenuService.java
+++ src/main/java/com/example/demo/menu/MenuService.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 @Service
 public class MenuService {
 
-    private final MenuRepository MenuRepository;
+    private final MenuRepository menuRepository;
 
     public MenuService(MenuRepository menuRepository) {
         this.menuRepository = menuRepository;
@@ -16,12 +16,12 @@ public class MenuService {
 
     // 저장
     public void register(MenuDto.Register dto) {
-        MenuRepository.save(dto.toEntity());
+        menuRepository.save(dto.toEntity());
     }
 
     // 단건 조회
     public MenuDto.Menu read(Integer id) {
-        Optional<MenuEntity> result = MenuRepository.findById(id);
+        Optional<MenuEntity> result = menuRepository.findById(id);
 
         if (result.isPresent()){
             MenuEntity entity = result.get();
@@ -34,7 +34,7 @@ public class MenuService {
 
     // 전체 조회
     public List<MenuDto.Menu> list() {
-        List<MenuEntity> result = MenuRepository.findAll();
+        List<MenuEntity> result = menuRepository.findAll();
         return result.stream().map(MenuDto.Menu::from).toList();
     }
 }
