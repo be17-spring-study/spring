507cfa90b4e302771f752f09b7c2e1b1948df763
sangwoo <lesw1216@gmail.com>
2025-08-06 12:18:49 +0900

    feat: product 컨트롤러 구현

--- src/main/java/com/example/demo/product/ProductController.java
+++ src/main/java/com/example/demo/product/ProductController.java
@@ -1,4 +1,48 @@
 package com.example.demo.product;
 
+import com.example.demo.common.BaseResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
 public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    // 저장
+    @PostMapping("/products")
+    public ResponseEntity<BaseResponse> productForm(@RequestBody ProductDto.Register create) {
+        productService.register(create);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("msg", "상품 등록 성공");
+        return ResponseEntity.ok().body(BaseResponse.success(map));
+    }
+
+    // 단건 조회
+    @GetMapping("/products")
+    public ResponseEntity<BaseResponse> getProduct(Integer id) {
+        ProductDto.Product findProduct = productService.read(id);
+
+        return ResponseEntity.ok().body(BaseResponse.success(findProduct));
+    }
+
+    // 전체 조회
+    @GetMapping("/products/list")
+    public ResponseEntity<BaseResponse> getProducts() {
+        List<ProductDto.Product> findProducts =  productService.list();
+
+        return ResponseEntity.ok().body(BaseResponse.success(findProducts));
+    }
 }
