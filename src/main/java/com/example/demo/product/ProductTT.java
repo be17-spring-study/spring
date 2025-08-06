46f13086bb2a1c5480592b795dc1c8baac24c963
young1042 <kyjfree00@gmail.com>
2025-08-06 12:04:28 +0900

    refactor;product entity수정

--- src/main/java/com/example/demo/product/ProductTT.java
+++ src/main/java/com/example/demo/product/ProductTT.java
@@ -12,6 +12,7 @@ public class ProductTT {
     private int id;
     private String name;
     private int price;
+    private String description;
 
     public int getId() {
         return id;
@@ -19,6 +20,14 @@ public class ProductTT {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public String getName() {
