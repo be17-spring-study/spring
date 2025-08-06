bd49a62c8344c767d091232dd35cf84c92d109fe
krh <fbsghksllove@gmail.com>
2025-08-06 11:55:31 +0900

    [feat] Dto, Service

--- src/main/java/com/example/demo/product/ProductService.java
+++ src/main/java/com/example/demo/product/ProductService.java
@@ -1,9 +1,14 @@
 package com.example.demo.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class ProductService {
+    @Autowired
     private final ProductRepository productRepository;
 
     public ProductService(ProductRepository productRepository){
