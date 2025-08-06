ee09870d5f617db8afb9813183a333453bf4885a
krh <fbsghksllove@gmail.com>
2025-08-06 16:36:00 +0900

    [Feat] BoardRepository/ MovieDto, MovieService

--- src/main/java/com/example/demo/menu/MenuController.java
+++ src/main/java/com/example/demo/menu/MenuController.java
@@ -1,11 +1,35 @@
 package com.example.demo.menu;
 
+import com.example.demo.test.ex03.Ex03Dto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/menu")
 public class MenuController {
+    private final MenuService menuService;
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody MenuDto.Register dto) {
+        menuService.register(dto);
+        return ResponseEntity.status(200).body("등록 성공");
+
+    }
+    @GetMapping("/list")
+    public ResponseEntity list() {
+        List<MenuDto.Menu> response = menuService.list();
+
+        return ResponseEntity.status(200).body(response);
+    }
+
+    @GetMapping("/read")
+    public ResponseEntity read(Integer idx) {
+        MenuDto.Menu response = menuService.read(idx);
+
+        return ResponseEntity.status(200).body(response);
+    }
 }
