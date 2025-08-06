14e37c144cf374addc992f5d55ca6cd01f4903c8
sangwoo <lesw1216@gmail.com>
2025-08-06 15:30:43 +0900

    feat: boardService 구현

---
+++ src/main/java/com/example/demo/board/BoardService.java
@@ -0,0 +1,40 @@
+package com.example.demo.board;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    public BoardService(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+    // 저장
+    public void register(BoardDto.Board dto) {
+        boardRepository.save(dto.toEntity());
+    }
+
+    // 단건 조회
+    public BoardDto.Board read(Integer id) {
+        Optional<BoardEntity> result = boardRepository.findById(id);
+
+        if (result.isPresent()){
+            BoardEntity entity = result.get();
+
+            return BoardDto.Board.from(entity);
+        }
+
+        return null;
+    }
+
+    // 전체 조회
+    public List<BoardDto.Board> list() {
+        List<BoardEntity> result = boardRepository.findAll();
+        return result.stream().map(BoardDto.Board::from).toList();
+    }
+}
